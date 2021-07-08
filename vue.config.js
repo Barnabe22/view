13d8ad5b99e4da3e8a47fec8257820fe2f29bef3
Barnabe222 <bigahou3@gmail.com>
2021-07-08 14:38:28 +0100

    changement du proxy en URL

--- vue.config.js
+++ vue.config.js
@@ -2,12 +2,14 @@
 
  module.exports = {
     devServer:{
-        proxy:{
+        proxy: 'http://neodfs.com:8080/questionnaire/'
+        
+        /*{
           '^/*': {
             target: 'http://216.137.189.207:8080/questionnaire/',
             ws: true,
             changeOrigin: true
           }
-        }
+        }*/
     }
   }
